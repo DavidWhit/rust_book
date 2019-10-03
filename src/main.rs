5179d3ccd8c1a1eaf35f1b1906a993f3a30ea268
David Whitaker <David.Whitaker@enovatemedical.com>
2019-10-03 17:33:10 -0500

    Updated until 4.2

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-
 fn main() {
-//    Chapter 3 variables
+    // +++++++++++++++++++++ Chapter 3 variables and Mutability +++++++++++++++++++++
 
     // LET vars ****************
 
     // the type is inferred from the right side
     // intellij shows the inferred type here
-    let a = 5;
+    let _a = 5;
 
     // default values are immutable thus
     // this would fail to compile a = 8;
 
     // you need to prefix the var mut to make it mutable
-    let mut y = 10;
-    y = 15; // This is ok
+    let mut _y = 10;
+    _y = 15; // This is ok
 
     // Constant vars *************
 
     // Constants cant be mut
-    // Constants by always be type annotated
+    // Constants must always be type annotated
     const MAX_POINTS: u32 = 100_000;
+
+    println!("Numbers with underscores for read {}", MAX_POINTS);
 
     // Note that we can use _ for readability
 
@@ -31,7 +32,7 @@ fn main() {
     let x = x + 1;
     let x = x * 2;
 
-    println!("the value of x is: {}",x);
+    println!("the value of x is: {}", x);
 
     // some other key differences
     // because we are technically creating a new variable
@@ -40,12 +41,12 @@ fn main() {
     let y = "Some string";
     let y = y.len();
 
-    println!("the value of y is: {}",y);
+    println!("the value of y is: {}", y);
 
     // ** you could not do the above with mut
     // because your not creating a new value
 
-    // 3.2 Data types
+    // +++++++++++++++++++++ 3.2 Data types +++++++++++++++++++++
 
     // scalar types Rust has 4 primary
     // integers
@@ -94,7 +95,7 @@ fn main() {
 
     // unlike integers floats default to f64
     let float_64 = 3.4;
-    let float_32 : f32 = 3.0;
+    let float_32: f32 = 3.0;
 
     // Numeric Operations
     // standard
@@ -114,24 +115,24 @@ fn main() {
 
     // Tuples
     // offer a way to group multiple types into one type
-    let tup: (i32,f64,u8) = (500,6.4,1);
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
     // can be accessed by destructuring
-    let (_,e,_) = tup;
+    let (_, e, _) = tup;
     // or access by position begins with 0
-    println!("{} {}",e, tup.2);
+    println!("{} {}", e, tup.2);
 
     //Arrays
     // Great for allocating data on the stack
     // vs the heap for known const data or when you want to ensure
     // you have a fixed number of elements.
     // literal
-    let a = [1,2,3,4,5];
+    let a = [1, 2, 3, 4, 5];
     // creat an array with 3, 5times
-    let a = [3;5];
+    let a = [3; 5];
     // accessing an index out of range will not be caught at compilation
     // however during runtime it will panic preventing invalid memory being accessed
 
-    // 3.3 Functions
+    // +++++++++++++++++++++ 3.3 Functions +++++++++++++++++++++
     // no support for default function arguments
     let result = another_function(32, 89);
     println!("result is: {}", result);
@@ -142,15 +143,15 @@ fn main() {
     //       and do not return a value.
     // Expressions evaluate to a resulting value
     let y = 6; // this is a statement
-    // calling a function
-    // calling a macro
-    // use {} to create a block are all 3 expressions
-    // expressions also do not end in ;
+               // calling a function
+               // calling a macro
+               // use {} to create a block are all 3 expressions
+               // expressions also do not end in ;
 
     let x = 10;
     let y = {
         // a block was created making a new scope but x is still 10 here
-        println!("x val inside block is: {}", x );
+        println!("x val inside block is: {}", x);
 
         let x = 3;
         // this is left with the ; off because we are saying this is an
@@ -162,18 +163,18 @@ fn main() {
 
     // we haven't shadowed x outside the block since the last 10
     // because y block {} created a new scope so x is 10 not 3
-    println!("x val outside block is: {}", x );
+    println!("x val outside block is: {}", x);
     println!("the value of y is {}", y);
 
-    // 3.4 Comments DUH begin with //
-    // 3.5 Control flow
+    // 3.4 +++++++++++++++++++++ Comments DUH begin with // +++++++++++++++++++++
+    // 3.5 +++++++++++++++++++++ Control flow +++++++++++++++++++++
 
     // Basic control structures like other langs
     let number = 9;
 
     // condition must be a boolean expression
     // exits the block on first true condition
-    // you can add this as a statement by adding;
+    // you can add this as a statement by adding ';'
     // so you could assign it to a let var
     // however each if case would need to be of the same type.
     let result = if number < 5 {
@@ -199,7 +200,7 @@ fn main() {
         if counter == 10 {
             // sorta confused here as this works
             // as an expression or statement.
-            break counter * 2
+            break counter * 2;
         }
     };
 
@@ -216,7 +217,7 @@ fn main() {
 
     // for keyword
     // obviously a better mechanic than using length and index counter
-    let a = [10,2,1,23,22,33,44];
+    let a = [10, 2, 1, 23, 22, 33, 44];
 
     for val in a.iter() {
         println!("{}", val);
@@ -228,15 +229,17 @@ fn main() {
         println!("{}!", number);
     }
 
-    //Chapter 4.1 Ownership
+    // +++++++++++++++++++++ Chapter 4.1 Ownership +++++++++++++++++++++
+
     // Each value in rust has a variable that's called its owner.
     // there can only be one owner at a time
     // when the owner goes out of scope the value will be dropped.
 
     // variable scope.
-    {  // begin scope
+    {
+        // begin scope
         let s = "hello block"; // s is valid
-        println!("{}",s);
+        println!("{}", s);
     } // scoped ended s is no longer valid
 
     // All previously covered data types are stored on the stack
@@ -297,7 +300,6 @@ fn main() {
     //                                  3        l
     //                                  4        o
 
-
     // length is how much memory in bytes the contents of String is currently using
     // capacity is the memory in bytes created by the OS
     // when we assign s1 to s2 the String data is copied meaning that copied
@@ -313,6 +315,7 @@ fn main() {
 
     // In Rust this is handled by invalidating the original s1.
     // So now Rust doesn't do any clean up if s1 goes out of scope.
+    // Also why this is called a mov from s1 to s2 and s1 is killed
 
     // This errors because it is invalid -- borrowed after moved
     // println!("{}",s1);
@@ -327,16 +330,164 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
 
-    println!("s1 = {} | s2 = {}", s1,s2);
+    println!("s1 = {} | s2 = {}", s1, s2);
 
     // Stack-Only Data: Copy
+    let x = 5;
+    let y = x;
+
+    // This seems to contradict what was just said but that is because these values are on
+    // the stack and these values are known at compile time. So there isn't a reason
+    // to drop x after creating y because there is no difference in a deep or shallow
+    // copy so calling clone wouldnt do anything different.
+
+    // Rust has a special annotation called the copy trait where we can place types
+    // like integers on the stack ** see chapter 10
+    // mutually exclusive -- if you have a drop trait you cant have a copy trait
+    // drop traits are on heap
+    // copy traits are on stack
+
+    // General Rule
+    // any group of simple scalar values can be Copy and nothing that requires allocation
+    // or is some form of resource is Copy. Here are some of the types that are Copy
+
+    // All integers, boolean, floats, char, tuples *if they only contain Copy trait
+    // implemented types
+    println!("x = {}, y = {}", x, y);
+
+    // ************* Ownership and Functions *************
+
+    // The same semantics for passing a value to a function are similar to those
+    // for assiging a value to a variable. That is...
+    // passing a variable with a drop trait will cause a move otherwise copy
+
+    let s = String::from("hello");
+    takes_ownership(s);
+
+    // s is not out of scope because it was moved
+    // println!("trying to call S will boom! {}", s);
+
+    let x = 5;
+
+    makes_copy(x);
+
+    println!("X is still here in scope: {}", x);
+
+    // x is still in scope because it was copied.
+
+    // ************* Return Values and Scope *************
+
+    //
+    let s1 = gives_onwership(); // function gave ownership via return val
+    let s2 = String::from("hello"); // set s2 on heap
+    println!("s2 -> {} original address: {:p}", s2, &s2);
+    let s2 = takes_and_gives_back(s2); // sent s2 to be modified and return back with concat
+    println!("s2 -> {} address: {:p}", s2, &s2); // you can see this is a new address
+    let s3 = takes_and_gives_back(s2); // moved s2
+                                       // s2 is now out of scope
+                                       // s3 holds final transform of s2
+                                       // no mutation has occured because you are returning a new value
+                                       // seen by the address changes
+    println!("s3 -> {} address: {:p}", s3, &s3);
+
+    // recap the ownership of a variable follows the same pattern every time
+    // assiging a value to another variable moves it
+    // when a variable that includes data on the heap goes out of scope, the value will be cleaned up by drop
+    // unless the data has been moved by another variable.
+
+    // This is obviously extremely tedious
+    // you could do things like tuple returns but references are best
+
+    let s1 = String::from("hello");
+    let (s1, len) = calculate_length(s1);
+
+    println!("s1 {} and length {}", s1, len);
+
+    // ************* 4.2 References and Borrowing *************
+
+    // As we just witnessed in the calculate_length fn above we had to use another variable
+    // in order to get the length otherwise a borrow after move error would occur
+    // WHY though?
+    // focus on the error it said "borrow after move"
+    // with the current signature return is -> (String, usize)
+    //
+    // ok that must mean we have already moved the value s1 and we next are trying to access on it via s.len()
+    // this must mean that if we reversed the return to get the length we would have done it successfully
+    // that is to say get the length first then move it.
+    // change fn calculate_length return to be -> (usize, String) and see it is fixed.
+    // again extremely tedious
+
+    // the alternative and I should say idiomatic way is to use references and borrowing
+
+    let s1 = String::from("Hello");
+    // s1 wasn't moved but borrowed by accessing the reference pointing to the location in memory
+    // on the heap.
+    // The below fn now has param &s that points to s1 that points to a location on the heap
+    // fn param &s points -> s1 points -> heap location
+    // notice the different addresses
+    let len = calculate_length// this is a mutable borrowing(&s1);
+    // s1 still in scope; printing
+    // because the function is holding a reference when it goes out of scope the value it points to will not.
+    println!("address of s1 {:p} {} {}", &s1, len, s1);
+
+    // reference &
+    // dereference *
+
+    // Alright lets try to modify something that is borrowed
+
+    // Good ideal concept is you can borrow something from a friend and you give it back when you're done.
+
+    let s = String::from("some test string");
+
+    // you can't mutate data that is referenced
+    //change(&s);
+
+    // so we have to add mutable
+    // this is a mutable reference
+
+    let mut s = String::from("some test string");
+
+    change(&mut s);
+
+    // one big restriction
+    //
 }
 
-
-// 3.3 function declaration
+// ************* 3.3 function declaration *************
 // uses snake case
 // can be at the top or bottom outside of main
 // example
-fn another_function (x: i32, y: i32) -> i32 {
+fn another_function(x: i32, y: i32) -> i32 {
     x + y
+}
+
+fn takes_ownership(some_sting: String) {
+    println!("{}", some_sting);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn gives_onwership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string + " with more"
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
+}
+
+fn calculate_length_borrowing(s: &String) -> usize {
+    println!("address of &s -> {:p}", &s);
+    s.len()
+} // s is out of scope
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", addition");
 }
