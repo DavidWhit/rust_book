c7d553b1091089c25be0cf3225228cbd49c72a1b
David Whitaker <David.Whitaker@enovatemedical.com>
2019-10-08 17:23:41 -0500

    upto chapter 5.3

--- src/main.rs
+++ src/main.rs
@@ -504,21 +504,247 @@ fn main() {
     // they are refs to a continguous sequence of elements in a collection rather than the whole collection.
 
     // making a function that returns the first word in a string.
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // s is now empty but word still holds 5
+
+    // compiles still but there is no lenght of 5 in s
+    // changing the return signature to usize, usize for start and end is even more error prone and requires more
+    // to keep in sync
+
+    // Enter Slices
+    // string slices are reference to part of a string
+
+    let s = String::from("Hello World");
+
+    let hello = &s[0..5]; // zero index and index based noninclusive for the top value 0-4
+    let world = &s[6..11];
+
+    let slice = &s[0..2]; // starting at zero \__  same result
+    let slice = &s[..2]; //  starting at zero /
+
+    // likewise to get the end you would start a x index &s[5..];
+    // or even take the whole string as &s[..]; or 0-length
+
+    /*
+    Note: String slice range indices must occur at valid UTF-8 character boundaries.
+    If you attempt to create a string slice in the middle of a multibyte character,
+    your program will exit with an error. For the purposes of introducing string
+    slices, we are assuming ASCII only in this section; a more thorough discussion
+    of UTF-8 handling is in the “Storing UTF-8 Encoded Text with Strings” section
+    of Chapter 8.
+    */
+
+    // with this let's rewrite the first_word function
+    // the logic works because we are tied to the underlying data
+    let x = first_word_redo(&s);
+
+    // now the values are maintained because we are ref the underlying value
+    // x is a pointer to the slice
+    // &s is a potiner to the String
+    println!("{}, {:p}, {:p}", s.len(), x, &s);
+
+    let s = "";
+
+    println!("{}", s.len());
+
+    // recalling immutable ref to something we cannot also take a mutable ref
+
+    let mut s = String::from("hello world");
+    let word = first_word_redo(&s);
+    // s.clear(); // error!
+    println!("the first word is: {}", word);
+
+    //     error[E0502]: cannot borrow `s` as mutable because it is also borrowed as immutable
+    //    --> src\main.rs:557:5
+    //     |
+    // 556 |     let word = first_word_redo(&s);
+    //     |                                -- immutable borrow occurs here
+    // 557 |     s.clear(); // error!
+    //     |     ^^^^^^^^^ mutable borrow occurs here
+    // 558 |     println!("the first word is: {}", word);
+    //     |                                       ---- immutable borrow later used here
+
+    // String literals are slices
+    let s = "Hello, world!";
+
+    // the type of s is &str and why string literals are immutable &str is an immutable reference
+
+    // String slices as params
+
+    // because of this we know can rewrite the first_word to handle both literals and strings
+    // by changing first_word_redo(s: &str) -> &str
+    // then you pass it the slice of the string or string literal (String - str)
+
+    // Other slices
+    let a: [i64; 5] = [3, 3, 2, 3, 4];
+
+    let sliceA = &a[2..5];
+
+    // CHAPTER 5 Defining and Instantiating Structs
+
+    // note that everything has to be mutable Rust doesn't allow specific fields to be mutable.
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusernamehere"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // uses dot notation like everything else
+
+    user1.email = String::from("anotheremail");
+
+    // creating instances from others with struct update syntax
+
+    let user2 = User {
+        email: String::from("another@exam2ple.com"),
+        username: String::from("anotherusername5647"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+
+    // we coul also create an instance and populate the remaining like so
+    let user3 = User {
+        email: String::from("another@exa3mple.com"),
+        username: String::from("anotherusername5637"),
+        ..user1
+    };
+
+    // Using tuple stucts without named Fields to create different types.
+    // See struct section
+    // although they have the same data types as inputs they are different my name
+    // that is a Color is not a Point and vice versa
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    // unit-like Structs are Structs without any fields
+    // this is because thye behave similarly to () the unit type
+    // Unit-like structs can be useful in situations in which you need to
+    // implement a trait on some type but don't have any data that you want to store in the type
+    /// itself MORE IN CHAPTER 10 traits
+    // Ownership of Struct Data
+    //
+    // in the examples we used the owned String type rather than the slice type &str
+    // this was to make sure we own all of its data and for that data to be valid for as long
+    // as the entire struct is valid.
+
+    // However, it is possible for structs to store refs to data owned by something else but to do so
+    // requires use of lifetimes
+
+    // Incorp functions
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    // refactored with tuples
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_tuple(rect1)
+    );
+
+    // adding more meaning
+    // added Rectangle Struct in struct section below
+
+    let rect1 = Rectangle {
+        height: 50,
+        width: 30,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect1)
+    );
+
+    // Adding useful functionality with derived traits
+
+    // struct types dont implement std::fmt::Display
+    // nor debug
+    // Rust does include functionality to print out debugging info, but we have to explicitly opt in to
+    // make that functionality avialable. To do that we add an annotation
+    // #[derive(Debug)] on the line before the struct definition
+    // no you should be able to access it with the debug trait
+    println!("{:?}", rect1);
+    // order add # for pretty print
+    println!("{:#?}", rect1);
+
+    // Implementing methods
+    // methods are simply functions defined within the contect of a struct, enum, or trait object
+    // and their first param is always self which is the instance of the struct in the method being called
+    // see imp below
+    // using the same Rectangle instance
+
+    println!("{:#?}", rect1.area());
+
+    // where is the -> operator
+    // Here’s how it works: when you call a method with object.something(),
+    // Rust automatically adds in &, &mut, or * so object matches the signature
+    // of the method. In other words, the following are the same:
+
+    // p1.distance(&p2);
+    // (&p1).distance(&p2);
+}
+
+// FUNCTIONS ********************************
+fn area_struct(r: &Rectangle) -> u32 {
+    r.height * r.width
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+// 5.1
+// will create a user with defaults and takes email and username
+fn build_user(email: String, username: String) -> User {
+    User {
+        // Note repeating the fields is tedious so you can do
+        // because it makes sense to name the params the same a the field
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
 
 // 4.3
+
+fn first_word_redo(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+// extremely brittle and error prone thus slices are introduced to avoid logic that trys to maintain sync
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     // iter() returns each element in the collection
-    // enumerate() wraps the result of iter and returns each element
+    // enumerate() wraps the result of iter and returns each element tuple of (index, ref) (mainly for getting the index)
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
+            // byte literal syntax
             return i;
         }
     }
 
-    s.len();
+    s.len()
 }
 
 // ************* 3.3 function declaration *************
@@ -559,3 +785,26 @@ fn calculate_length_borrowing(s: &mut String) -> usize {
 fn dangle() -> String {
     String::from("hello")
 } // now s is out of scope and dropped the easy way to resolve this is to move it
+
+// STRUCTS **************************************************
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
