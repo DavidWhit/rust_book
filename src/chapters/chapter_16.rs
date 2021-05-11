ca300c8711769d145929588d9f599c88e09fd451
DavidWhitaker <davidwhit5@gmail.com>
2021-05-11 18:51:00 -0500

    missing additons from uncommit from home

--- src/chapters/chapter_16.rs
+++ src/chapters/chapter_16.rs
@@ -1,8 +1,7 @@
-
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc;
-use std::sync::Mutex;
 
 pub fn fearless_concurrency() {
     /*
@@ -14,8 +13,8 @@ pub fn fearless_concurrency() {
 
     Rust devs found that ownership and type systems are powerful set of tools to help manage memory
     saftey and concurrency problems!
-    
-    This allows catching a lot of bugs before being release to production by getting catch by the 
+
+    This allows catching a lot of bugs before being release to production by getting catch by the
     compiler; thus, dubbed fearless
 
     For the sake of simplicity the rest of the chapter refs problems as concurrent rather than being more
@@ -30,8 +29,6 @@ pub fn fearless_concurrency() {
         well as types provided by the standard library.
     */
 
-
-
     /*
     * In most programs executed code runs in a process and the OS manages those at once.
     * Within your program you can also have independent parts that run simultaneously.
@@ -43,35 +40,34 @@ pub fn fearless_concurrency() {
       the program does multiple tasks at the same time, but it also adds complexity. Threads can run
       simultaneously, there's no inherent guarantee about the order in which parts of your code run on
       different threads. This causes problems like:
-        Race conditions, 
+        Race conditions,
         Dead locks
         Bugs -- Something that is hard to replicate in a threaded environment
 
       Rust trys to mitigate the negative use of threads but programming a multi threaded context still takes
       careful thought and requries a code structure that is different from that in programs running  a single thread.
 
-      Rust implements threads in a few different ways. 
+      Rust implements threads in a few different ways.
       Many OS's provide API for creating new threads often called 1:1 meaning one OS thread per language thread
 
-      Then programming languages provide their own special implementation of threads. 
-      Programming language provided threads are known as green threads and run in the context of a different number of 
+      Then programming languages provide their own special implementation of threads.
+      Programming language provided threads are known as green threads and run in the context of a different number of
       operating system threds. For this reason green-threaded model is called the M:N model there are (M) green threads
-      per (N) operating system threads where M and N are not necessarily the same number 
+      per (N) operating system threads where M and N are not necessarily the same number
 
-      Each model has it's own advantages and disadvantages and the disadvantage most important to Rust is 
+      Each model has it's own advantages and disadvantages and the disadvantage most important to Rust is
       runtime support. Runtime is confusing as it can have different meanings in different contexts.
 
-      In this context by runtime we mean code that is included by the language in every binary; larger or small. 
-      Every non-assembly language will have some amoutn of runtime code. Colloquially when people say a language has 
+      In this context by runtime we mean code that is included by the language in every binary; larger or small.
+      Every non-assembly language will have some amoutn of runtime code. Colloquially when people say a language has
       "no runtime" they often mean "samm runtime" Smaller runtimes = fewer features but result in smaller binaries
       which can combine with other languages in more contexts. Rust neds to have nearly no runtime and cannot compromise on
       being able to call into C to maintain performance.
 
       The green-threaded M:N model requires larger language run times to manage threads as such Rust std::lib onlyr provides
-      implementation of 1:1 trheading. Though there exists crates that implement M:N threading for the cost of overhead. 
+      implementation of 1:1 trheading. Though there exists crates that implement M:N threading for the cost of overhead.
       For aspects such as more control over which threads run when and lower costs of context switching,
     */
-
 
     let handle = thread::spawn(|| {
         for i in 1..10 {
@@ -93,23 +89,23 @@ pub fn fearless_concurrency() {
     // notice from the above that the main thread can close before the spawned thread completes
     // if the handle code is removed.
     // to resolve this by saving the handle added above and joining it back to the main thread.
-    // thread::spawn return type is a JoinHandle which is an owned value 
+    // thread::spawn return type is a JoinHandle which is an owned value
     // HOWEVER this causes the code to wait for the spawned thread
-    // IF positioned after the spawned thread which doesn't make a lot of sense unless you 
+    // IF positioned after the spawned thread which doesn't make a lot of sense unless you
     //      to wait for all threads to finish independantly join at the end
     //      then the time is the slowest thread
 
     // *** using move closures with threads
-    // the move closure is often used alongside thread::spawn because it allows you to use data 
+    // the move closure is often used alongside thread::spawn because it allows you to use data
     // from another thread.
 
     // in chapter 13 we mentioned we can use the move before the param list of a closure to
-    // force the closure to take ownership of the values it uses in the environment. 
-    // especially used 
+    // force the closure to take ownership of the values it uses in the environment.
+    // especially used
 
-    let v = vec![1,2,3];
+    let v = vec![1, 2, 3];
 
-    let handle = thread::spawn( move || {
+    let handle = thread::spawn(move || {
         // grants us certainity that it cant be used again outside the spawned thread
         // check drop(v)
         println!("here's a vector: {:?}", v);
@@ -117,38 +113,37 @@ pub fn fearless_concurrency() {
 
     // drop(v);
 
-    // Rust infers how to capture v, and because println! only needs a reference to v, the closure 
-    // tries to borrow v. However, there’s a problem: Rust can’t tell how long the spawned thread 
+    // Rust infers how to capture v, and because println! only needs a reference to v, the closure
+    // tries to borrow v. However, there’s a problem: Rust can’t tell how long the spawned thread
     // will run, so it doesn’t know if the reference to v will always be valid.
     // *** add move to the closure to force it take ownership of v
 
     handle.join().unwrap();
-
 }
 
 pub fn using_msg_passing_to_trns_data_btwn_threads() {
     /*
     An increasingly popular approach to ensure safe concurrency is message passing
-    where threads or actors communicate by sending each other messages containing data. 
+    where threads or actors communicate by sending each other messages containing data.
     The idea slogan from "Go lang"  "Do not communicate by sharing memory; instead
     share memory by commmunicating."
 
     The major tool Rust has for accomplishing message-sending concurrency is the channel
-    a concept that the std lib provides. 
+    a concept that the std lib provides.
 
     Think of it as a channel of water such as a stream or a river. If you put something
     like a rubber duck or boat into a stream, it will travel downstream to the end of the
     waterway.
 
     A channel has two halves a transmitter and a receiver
-    The transmitter half is upstream sending them 
-    The reciever half is downstream  receiving 
+    The transmitter half is upstream sending them
+    The reciever half is downstream  receiving
     The channel is closed if either the transmitter or receiver half is dropped.
 
-    The example will create a program that has one thread to generate values and send them 
-    down a channel and thread that will receive the values and print them out. 
+    The example will create a program that has one thread to generate values and send them
+    down a channel and thread that will receive the values and print them out.
 
-    We'll be sending simple values between threads using a channels 
+    We'll be sending simple values between threads using a channels
     */
 
     // mpsc == multi producer single consumer
@@ -158,11 +153,11 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
     thread::spawn(move || {
         let val = String::from("hi");
         tx.send(val).unwrap(); // closure has ownership of tx via move and then send takes ownership
-        // println!("Got: {}", val);  val is no longer available
+                               // println!("Got: {}", val);  val is no longer available
     });
 
     let received = rx.recv().unwrap(); // rx.recv blocks the main thread and returns Result<T,E>
-    // try_recv is useful if the thread has other work to do while waiting for messages
+                                       // try_recv is useful if the thread has other work to do while waiting for messages
     println!("Got: {}", received);
 
     //***  Channels and Ownership Transference
@@ -175,7 +170,7 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
             String::from("hi"),
             String::from("from"),
             String::from("the"),
-            String::from("thread")
+            String::from("thread"),
         ];
 
         for val in vals {
@@ -189,7 +184,7 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
             String::from("more"),
             String::from("messages"),
             String::from("for"),
-            String::from("you")
+            String::from("you"),
         ];
 
         for val in vals {
@@ -198,13 +193,11 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
         }
     });
 
-
-    // The sends from above are going to make 
+    // The sends from above are going to make
     // again the main thread is blocked until all messages are received
     for received in rx {
         println!("Got: {}", received);
     }
-
 
     /* *** Shared-State Concurrency
     msg passing is a fine way handling concurrency but its not the only one
@@ -213,7 +206,7 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
 
     channels in any programming language are similar to single ownership, because once
     you transfer a value down a channel you should no longer use that value.
-    shared memory is having multiple ownership where mult threads can access the same 
+    shared memory is having multiple ownership where mult threads can access the same
     memory location this is more difficult because multiple owners need managing
     Rusts ownership and rules greatly assit in getting this management correct.
 
@@ -249,11 +242,77 @@ pub fn using_msg_passing_to_trns_data_btwn_threads() {
         // lock will block the current thread (this case the main thread)
         // the call to lock would fail if another thread holding the lock panicked
         // the call to lock returns a type MutexGuard
-        // it implements Deref, Drop to point to the inner data 
+        // it implements Deref, Drop to point to the inner data
         let mut num = m.lock().unwrap();
         *num = 6;
-    }// lock is dropped after scope
+    } // lock is dropped after scope
 
     println!("m = {:?}", m);
 
+    // *** Sharing a Mutex<T> between threads
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    // create 10 threads
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        // we can't do this because we moved the ownership on the previous version of
+        // the loop: Recall back to chapter 15 about Rc<T>
+        // compiler catches another issue that it can't safely do this
+        // and further that Rc<Mutex<i32>> doesn't implement Send trait
+        // Send ensures the types we use with threads are meant for use in
+        // conncurrent situations in which Rc<T> is not mean to share thus no Send trait
+
+        // so what can we do?
+        // Arc<T> is like Rc<T> but it is safe to use in concurrent situations
+        // the A stands for Atomic meaning it's an atomically reference counted type
+        // std::sync::atomic for more information
+
+        //So why all primitives types aren't atomic and why the std lib types aren't
+        // implemented to use Arc<T>. For this reason thread safety comes with a
+        // performance penalty that you only want to pay when you really need to.
+
+        // going back to the code after importing Arc
+        let handle = std::thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        // finishes closure lock is released because of scope
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result:{}", *counter.lock().unwrap());
+
+    // *** multiple ownership with multiple threads
+    // see changes above with Arc
+
+    // *** conclusion
+    /*
+    Using this strategy, you can divide a calculation into independent parts,
+    split those parts across threads, and then use a Mutex<T> to have each
+    thread update the final result with its part.
+
+
+    Notice that counte ris immutable but we got a mutable reference
+        let counter = Arc::clone(&counter);
+
+    Meaning Mutex<T> provides interior mutability as the Cell family does
+    In the same way RefCell<T> allows us to mutate contents inside an Rc<T>
+    We used Mutext<T> to mutate contents inside an Arc<T>
+
+
+    Also just like Rust can't protect against Rc<T> having ref cycles
+    when lead to memory leaks
+
+    Mutext<T> comes with the risk of deadlocking when an operation needs to lock
+    two resources and two threads have each acquired one of the locks causing them to
+    wait for each other forever.
+    */
 }
